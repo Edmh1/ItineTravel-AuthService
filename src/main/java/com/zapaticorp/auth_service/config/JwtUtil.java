a4e1a428c0a9ed3b738f7f4a53b637a77600e80c
Edmh <edavidmanotas@gmail.com>
2025-09-27 18:12:22 -0500

    feat: crea beans de jwtUtil y conf de seguridad

---
+++ src/main/java/com/zapaticorp/auth_service/config/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.zapaticorp.auth_service.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private static final long EXPIRATION_TIME = 3600000; // 1 hora
+
+    // Genera un token
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, secretKey.getBytes())
+                .compact();
+    }
+}
